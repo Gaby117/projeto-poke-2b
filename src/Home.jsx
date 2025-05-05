1a58a5c1db5700479b52d5fb86f4831eab685626
Gaby117 <annagaby.0309@gmail.com>
2025-05-05 12:30:03 -0300

    imagempoke

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import poke from "./assets/pokeapi_256.png"
+
+const Home = ()=> {
+    return (
+        <div className="pokeflex">
+            <img scr={poke} />
+           <img scr={poke} />
+           <img scr={poke} />
+           <img scr={poke} />
+            <img scr={poke} />
+            <img scr={poke} />
+         </div>
+    )
+}
+
+export default Home
